7b773172e9f1c9c87eef3553dfc289e3e5854364
niuniuanran <irene.anran.niu@gmail.com>
2020-09-20 16:05:37 +1200

    Correct get handler

--- get.js
+++ get.js
@@ -1,9 +1,7 @@
 import handler from "./libs/handler-lib";
 import dynamoDb from "./libs/dynamodb-lib";
 
-// main is a function returned by the handler wrapper.
-export const main = handler(async (event, _) => {
-
+export const main = handler(async (event, context) => {
     const params = {
         TableName: process.env.tableName,
         // 'Key' defines the partition key and sort key of the item to be retrieved
@@ -13,13 +11,13 @@ export const main = handler(async (event, _) => {
             userId: event.requestContext.identity.cognitoIdentityId,
             noteId: event.pathParameters.id
         }
-    }
+    };
 
     const result = await dynamoDb.get(params);
-
     if ( ! result.Item) {
         throw new Error("Item not found.");
     }
 
+    // Return the retrieved item
     return result.Item;
-})
+});
